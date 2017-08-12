34de7ab91947613ee30b3b1a43093354fcb3b9e2
caleb logan <clogan202@gmail.com>
2017-08-12 16:18:15 -0700

    added twitterscheduler app

--- twitter_site/settings.py
+++ twitter_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'twitterscheduler.apps.TwitterschedulerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'twitter_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'twitter_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitterscheduler',
+        'USER': os.environ['PSQL_USER'],
+        'PASSWORD': os.environ['PSQL_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ twitterscheduler/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+
+from . import views
+
+app_name = 'twitterscheduler'
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
