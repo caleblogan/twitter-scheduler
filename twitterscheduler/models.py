546ff329b525fd27b0cc7687f876f57848b45f31
caleb logan <clogan202@gmail.com>
2017-08-14 13:40:27 -0700

    added models

--- twitterscheduler/models.py
+++ twitterscheduler/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    require_correctly_spelled = models.BooleanField(default=False)
+    require_positive_sentiment = models.BooleanField(default=False)
+    last_sync_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user} ({self.last_sync_time})'
+
+
+class Tweet(models.Model):
+    tweet_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=140)
+
+    sentiment_choices = (
+        ('p', 'positive'),
+        ('n', 'negative'),
+        ('u', 'unknown')
+    )
+    sentiment = models.CharField(max_length=1, choices=sentiment_choices, blank=True, default='u', help_text='Sentiment of tweet')
+    time_posted_at = models.DateTimeField(null=True, blank=True)
+    is_posted = models.BooleanField(default=False, help_text='Whether or not the tweet has been posted to twitter')
+
+    def __str__(self):
+        return f'{self.user} - {self.text}'
+
+
+class ScheduledTweet(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    time_to_tweet = models.DateTimeField()
--- twitterscheduler/views.py
+++ twitterscheduler/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-from allauth.socialaccount.models import SocialToken
+from allauth.socialaccount.models import SocialToken, SocialApp
+
+import tweepy
 
 
 @login_required
 def index(request):
-
-    print(request.user)
     access_token = get_object_or_404(SocialToken, account__user=request.user, app__provider='twitter')
-    print(f'access_token: {access_token}')
-    return render(request, 'twitterscheduler/index.html', context={'access_token': access_token})
+    # token, token_secret = access_token.token, access_token.token_secret
+    print(f'user: {request.user}')
+    print(f'access_token: {access_token.token}')
+
+
+    context = {
+        'access_token': access_token,
+        # 'user_tweets': user_tweets,
+    }
+
+    return render(request, 'twitterscheduler/index.html', context=context)
+
+
+def sync_tweets_twitter(access_token, token_secret):
+    """Gets the users tweets from twitter and saves them to db."""
+    twitter_api = get_authed_tweepy(access_token, token_secret)
+
+    public_tweets = twitter_api.user_timeline()
+    if public_tweets:
+        return JsonResponse({'tweet': public_tweets[0]._json})
+
+
+def get_authed_tweepy(access_token, token_scret):
+    """Returns an authed instance of the twitter api wrapper tweepy for a given user."""
+    social_app_twitter = get_object_or_404(SocialApp, provider='twitter')
+
+    auth = tweepy.OAuthHandler(social_app_twitter.client_id, social_app_twitter.secret)
+    auth.set_access_token(access_token, token_scret)
+    return tweepy.API(auth)
