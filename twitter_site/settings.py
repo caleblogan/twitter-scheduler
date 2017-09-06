333eee11399a70a15eedb096b1dc3a2c51d40f38
caleb logan <clogan202@gmail.com>
2017-09-05 19:04:19 -0700

    added django-crispy-forms

--- twitter_site/settings.py
+++ twitter_site/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'twitterscheduler.apps.TwitterschedulerConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -150,6 +151,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/scheduler/'
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # CELERY_BROKER_URL = 'amqp://guest:guest@rabbit:5672/'
 # CELERY_BROKER_URL = 'amqp://guest:guest@192.168.99.100:5672/'
@@ -160,3 +163,6 @@ CELERY_BROKER_URL = 'amqp://{user}:{password}@{hostname}:{port}/'.format(
     port=get_from_env('RABBITMQ_PORT', '5672'),
 )
 CELERY_TIMEZONE = TIME_ZONE
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- twitter_site/urls.py
+++ twitter_site/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/scheduler/', permanent=True)),
     url(r'^scheduler/', include('twitterscheduler.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
